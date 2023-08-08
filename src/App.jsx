e61d7ac83f4524e9b3f2570d64ecbd071a506127
khoidang2110 <khoidang2110@gmail.com>
2023-08-08 23:44:04 +0700

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "./data/shoes.json";
 import Main from "./components/Main";
 import Basket from "./components/Basket";
@@ -13,9 +13,11 @@ function App() {
         x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
       );
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
     } else {
       const newCartItems = [...cartItems, { ...product, qty: 1 }];
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
     }
   };
   const onRemove = (product) => {
@@ -23,18 +25,24 @@ function App() {
     if (exist.qty === 1) {
       const newCartItems = cartItems.filter((x) => x.id !== product.id);
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
     } else {
       const newCartItems = cartItems.map((x) =>
         x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
       );
       setCartItems(newCartItems);
+      localStorage.setItem('cartItems',JSON.stringify(newCartItems));
     }
   };
   const onDelete = (product) => {
        const newCartItems = cartItems.filter((x) => x.id !== product.id)
    setCartItems(newCartItems);
+   localStorage.setItem('cartItems',JSON.stringify(newCartItems));
   }
 
+  useEffect(()=>{
+setCartItems(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []);
+  },[])
   return (
     <>
       <body>
